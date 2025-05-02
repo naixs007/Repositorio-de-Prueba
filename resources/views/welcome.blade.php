fbc36cfbf8b0ef4821560a22887afe1a61489a0c
Bryan <b.a.h.007@live.com>
2025-05-02 15:42:00 -0400

    Primer commit en el proyecto de prueba

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistema de Información 1</title>
+</head>
+
+<body>
+    <label for="">LABEL</label>
+
+    <input type="text" placeholder="introduce tu nombre">
+    <br>
+    <br>
+    <input type="password">
+    <br>
+    <br>
+    <label for="">
+        <input type="checkbox"> yes
+    </label>
+    <br>
+    <input type="checkbox"> no
+    <br>
+    <input type="radio">
+    <br>
+
+    <button>Enviar</button>
+</body>
+
+</html>
