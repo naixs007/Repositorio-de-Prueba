c42e11f24646e733c902d07d110e312111bfde4e
Bryan <b.a.h.007@live.com>
2025-05-03 13:41:44 -0400

    se añadio nuevas lineas al label

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
     <label for=""> Esto es un LABEL y es de prueba</label>
 
     <input type="checkbox"> se creo un nuevo checkbox
-    <label for=""> label para probar</label>
+    <label for=""> label para probar y nueva linea</label>
 </body>
 
 </html>
